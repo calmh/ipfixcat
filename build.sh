168946e68257ff71add3483955bba502963f643c
Jakob Borg <jakob@nym.se>
2013-07-22 14:32:50 +0200

    Include readme in build

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ go install github.com/calmh/ipfixcat
 mkdir $name
 cp $GOPATH/bin/ipfixcat $name
 cp $GOPATH/src/github.com/calmh/ipfixcat/*-fields.ini $name
+cp $GOPATH/src/github.com/calmh/ipfixcat/README.md $name
 tar zcvf $name.tar.gz $name
 rm -r $name
