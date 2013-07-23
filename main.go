c62f947ee2200f2330b55504621be0f46300ebc8
Jakob Borg <jakob@nym.se>
2013-07-23 12:53:54 +0200

    stats

--- main.go
+++ main.go
@@ -5,10 +5,41 @@ import (
 	"flag"
 	"fmt"
 	"github.com/calmh/ipfix"
+	"log"
 	"os"
+	"time"
 )
 
+const (
+	statReportInterval = 60
+)
+
+func messagesGenerator(s *ipfix.Session) <-chan map[string]interface{} {
+	c := make(chan map[string]interface{})
+
+	go func() {
+		for {
+			msg, err := s.ReadMessage()
+			if err != nil {
+				panic(err)
+			}
+
+			for _, record := range msg.DataRecords {
+				set := make(map[string]interface{})
+				set["templateId"] = record.TemplateId
+				set["exportTime"] = msg.Header.ExportTime
+				set["elements"] = s.Interpret(&record)
+
+				c <- set
+			}
+		}
+	}()
+
+	return c
+}
+
 func main() {
+	log.Println("ipfixcat", ipfixcatVersion)
 	dictFile := flag.String("dict", "", "User dictionary file")
 	flag.Parse()
 
@@ -18,24 +49,26 @@ func main() {
 		loadUserDictionary(*dictFile, s)
 	}
 
+	msgC := messagesGenerator(s)
+	tick := time.Tick(time.Duration(statReportInterval) * time.Second)
+	t0 := time.Now()
+	records := 0
 	for {
-		msg, err := s.ReadMessage()
-		if err != nil {
-			panic(err)
-		}
-
-		for _, record := range msg.DataRecords {
-			set := make(map[string]interface{})
-			set["templateId"] = record.TemplateId
-			set["exportTime"] = msg.Header.ExportTime
-			set["elements"] = s.Interpret(&record)
-
-			bs, err := json.Marshal(set)
+		select {
+		case msg := <-msgC:
+			records++
+			bs, err := json.Marshal(msg)
 			if err != nil {
 				panic(err)
 			}
-
 			fmt.Println(string(bs))
+		case <-tick:
+			now := time.Now()
+			diff := now.Sub(t0).Seconds()
+			recsPs := float64(records) / diff
+			log.Printf("%d records (%3.2f/s)\n", records, recsPs)
+			t0 = now
+			records = 0
 		}
 	}
 }
