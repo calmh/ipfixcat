5ef83af15331368313ae65ed9d8754f97f81f8bd
Jakob Borg <jakob@nym.se>
2014-07-30 10:52:14 +0200

    Solaris build, version in name

--- build.sh
+++ build.sh
@@ -3,18 +3,18 @@
 BIN=ipfixcat
 VER=$(git describe --always)
 
+rm -f *.tar.gz *.zip
 for GOARCH in amd64 386 ; do
-	for GOOS in darwin linux freebsd freebsd ; do
+	for GOOS in darwin linux freebsd solaris ; do
 		export GOOS
 		export GOARCH
 
-		NAME="$BIN-$GOOS-$GOARCH"
+		NAME="$BIN-${VER}_$GOOS-$GOARCH"
 		rm -rf "$NAME" "$NAME.tar.gz" "$BIN" "$BIN.exe"
 		go build -ldflags "-X main.ipfixcatVersion ${VER}"
 
 		mkdir "$NAME"
-		cp *.ini ipfixcat "$NAME"
-		tar zcf "$NAME.tar.gz" "$NAME"
+		cp *.ini ipfixcat "$NAME" && tar zcf "$NAME.tar.gz" "$NAME"
 		rm -r "$NAME"
 	done
 
@@ -22,13 +22,12 @@ for GOARCH in amd64 386 ; do
 		export GOOS
 		export GOARCH
 
-		NAME="$BIN-$GOOS-$GOARCH"
+		NAME="$BIN-${VER}_$GOOS-$GOARCH"
 		rm -rf "$NAME" "$NAME.tar.gz" "$BIN" "$BIN.exe"
 		go build -ldflags "-X main.ipfixcatVersion ${VER}"
 
 		mkdir "$NAME"
-		cp *.ini ipfixcat.exe "$NAME"
-		zip -r "$NAME.zip" "$NAME"
+		cp *.ini ipfixcat.exe "$NAME" && zip -r "$NAME.zip" "$NAME"
 		rm -r "$NAME"
 	done
 done
